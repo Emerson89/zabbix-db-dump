7e0ff3dd77a750212cd9c29cbe1a450b4b46c532
Emerson <51987534+Emerson89@users.noreply.github.com>
2022-08-24 18:08:03 -0300

    Update dump_script_mysql.sh

--- dump_script_mysql.sh
+++ dump_script_mysql.sh
@@ -4,6 +4,6 @@ DBNAME=NAMEDB
 DBUSER=USERBANCO
 DBPASS=SENHABANCO
 
-mysqldump --no-tablespaces -u"$DBUSER" -p"$DBPASS" "$DBNAME" --single-transaction | gzip > "$DBNAME-`date +%Y-%m-%d`.sql.gz"
+mysqldump --no-tablespaces -u"$DBUSER" -p"$DBPASS" "$DBNAME" --single-transaction | gzip > "$DBNAME-`date +%Y-%m-%d-%H-%M`.sql.gz"
 
 echo "=======Backup Realizado======"
